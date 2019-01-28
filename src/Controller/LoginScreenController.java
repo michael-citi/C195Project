9c5583418f8a5556fb1ab5eb81a21a2d8144b53e
Michael <mciti5211@gmail.com>
2019-01-27 18:35:50 -0800

    First Commit

---
+++ src/Controller/LoginScreenController.java
@@ -0,0 +1,117 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Modality;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.Locale;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class LoginScreenController implements Initializable {
+
+    // determining system locale and translation
+    private Locale locale = Locale.getDefault();
+    private ResourceBundle messages = ResourceBundle.getBundle("Translations.Bundle", locale);
+
+    // declare scene variables
+    @FXML private TextField uNameTextField;
+    @FXML private PasswordField pWordTextField;
+    @FXML private Button cancelBtn;
+    @FXML private Button loginBtn;
+    @FXML private Label uNameLabel;
+    @FXML private Label pWordLabel;
+    @FXML private Label welcomeLabel;
+
+    // login credentials
+    private static final String uName = "test";
+    private static final String pWord = "test";
+
+    // track login attempts
+    private static int loginCounter = 0;
+
+    @FXML
+    private void checkCredentials() {
+        String tempUserName = uNameTextField.getText();
+        String tempPassword = pWordTextField.getText();
+
+        if (loginCounter < 3) {
+            // check username and password values against valid credentials
+            if (uName.equals(tempUserName) && pWord.equals(tempPassword)) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle(messages.getString("successLoginTitle"));
+                alert.setHeaderText(messages.getString("successLoginHeader"));
+                alert.setContentText(messages.getString("successLoginContent"));
+                alert.initModality(Modality.WINDOW_MODAL);
+                alert.showAndWait();
+                // execute scene transition to main screen
+                login();
+            } else {
+                // return error and increment brute force protection
+                loginCounter += 1;
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(messages.getString("failedLoginErrorHeader"));
+                alert.setTitle(messages.getString("failedLoginErrorTitle"));
+                alert.setContentText(messages.getString("failedLoginErrorContent"));
+                alert.initModality(Modality.APPLICATION_MODAL);
+                alert.showAndWait();
+            }
+        } else if (loginCounter == 3) {
+            // brute force protection
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(messages.getString("forceCloseErrorTitle"));
+            alert.setHeaderText(messages.getString("forceCloseErrorHeader"));
+            alert.setContentText(messages.getString("forceCloseErrorContent"));
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.showAndWait();
+            System.exit(0);
+        }
+    }
+
+    private void login() {
+        //TODO: Scene transition to main screen.
+        System.out.println("Login successful");
+    }
+
+    @FXML
+    private void cancel() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(messages.getString("confirmExitTitle"));
+        alert.setHeaderText(messages.getString("confirmExitHeader"));
+        alert.setContentText(messages.getString("confirmExitContent"));
+        alert.initModality(Modality.WINDOW_MODAL);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == ButtonType.OK) {
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // assign translated text to buttons and labels
+        cancelBtn.setText(messages.getString("cancelBtn"));
+        loginBtn.setText(messages.getString("loginBtn"));
+        uNameLabel.setText(messages.getString("username"));
+        pWordLabel.setText(messages.getString("password"));
+        welcomeLabel.setText(messages.getString("welcomeLabel"));
+
+        // establish SQL database connection
+        Connection dbConnection = null;
+        String dbURL = "jdbc:mysql://52.206.157.109/";
+        String dbUser = "U04V6U";
+        String dbPass = "53688353202";
+
+        // error control
+        try {
+            dbConnection = DriverManager.getConnection(dbURL, dbUser, dbPass);
+            System.out.println("Connected to database.");
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Model/Users.java
@@ -0,0 +1,108 @@
+package Model;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Modality;
+
+public class Users {
+    // user properties
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String address;
+    
+    // constructor with basic user validation built-in
+    public Users(String firstName, String lastName, String phoneNumber, String address) {
+        switch (validateUserInfo(firstName, lastName, phoneNumber, address)) {
+            case 0:
+                failedAlert("First Name");
+                break;
+            case 1:
+                failedAlert("Last Name");
+                break;
+            case 2:
+                failedAlert("Phone Number");
+                break;
+            case 3:
+                failedAlert("Address");
+                break;
+            case 4:
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.phoneNumber = phoneNumber;
+                this.address = address;
+                successAlert();
+            default:
+                System.out.println("Unknown/Invalid User Creation.");
+        }
+    }
+
+    // getters & setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    // validating user info before creating user
+    // very basic validation. Regex comparisons will be used if needed.
+    private int validateUserInfo(String fName, String lName, String phoneNumber, String address) {
+        if(fName.equals("")) {
+            return 0;
+        } else if(lName.equals("")) {
+            return 1;
+        } else if (phoneNumber.equals("") || (phoneNumber.length() != 7) ) {
+            return 2;
+        } else if (address.equals("")) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+    
+    // generated error message
+    private void failedAlert(String property) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("User creation failed.");
+        alert.setContentText("The user info: " + "\"" + property + "\"" + " is invalid.\n\n Please try again.");
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.showAndWait();
+    }
+    
+    // method created to reduce clutter in constructor
+    private void successAlert() {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("User Created");
+        alert.setHeaderText("Success!");
+        alert.setContentText("User: " + lastName + ", " + firstName + " has been successfully created!");
+        alert.initModality(Modality.NONE);
+        alert.showAndWait();
+    }
+}
