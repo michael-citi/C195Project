86b330999ff8b03e253602892fb38f023d56dd20
michael-citi <mciti5211@gmail.com>
2019-02-11 15:44:38 -0800

    updating methods across scenes

--- src/Controller/UserListController.java
+++ src/Controller/UserListController.java
@@ -18,21 +18,21 @@ import Model.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
 
 public class UserListController implements Initializable {
 
     // table elements
     @FXML private TableView<Users> userTable;
-    @FXML private TableColumn<Users, String> fullNameCol;
     @FXML private TableColumn<Users, String> userNameCol;
-    @FXML private TableColumn<Users, String> phoneNumberCol;
     @FXML private TableColumn<Users, Integer> activeCol;
     
     @FXML private TextField userSearchTextField;
@@ -52,14 +52,49 @@ public class UserListController implements Initializable {
     
     // remove specific user
     @FXML
-    private void removeUser() {
-        
+    private void removeUser() throws SQLException {
+        // prevent action if no user is selected
+        if (userTable.getSelectionModel().getSelectedItem() == null) {
+            selectError();
+        } else {
+            // confirmation alert
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirm Delete");
+            alert.setContentText("Please confirm deleting user: " + userTable.getSelectionModel().getSelectedItem().getUserName());
+            alert.initModality(Modality.APPLICATION_MODAL);
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == ButtonType.OK) {
+                // sql delete statement
+                PreparedStatement statement = null;
+                String query = "DELETE FROM user, appointment "
+                        + "WHERE user.userId = ?";
+                try {
+                    statement = LoginScreenController.dbConnect.prepareStatement(query);
+                    statement.setInt(1, userTable.getSelectionModel().getSelectedItem().getUserId());
+                    statement.executeUpdate();
+                } catch (SQLException ex) {
+                    Logger.getLogger(UserListController.class.getName()).log(Level.SEVERE, null, ex);
+                } finally {
+                    if (statement != null) {
+                        statement.close();
+                    }
+                }
+            }
+        }
     }
     
     // load Modify User screen with user data
     @FXML
     private void modUser(ActionEvent event) throws IOException {
-        loadScene(event, "/View/ModUser.fxml");
+        // prevent action if no user is selected
+        if (userTable.getSelectionModel().getSelectedItem() == null) {
+            selectError();
+        } else {
+            // assign user variable to be transferred to modify user screen
+            transitionUser = userTable.getSelectionModel().getSelectedItem();
+            loadScene(event, "/View/ModUser.fxml");
+        }
     }
     
     // load New User screen
@@ -91,7 +126,7 @@ public class UserListController implements Initializable {
         UserListController.transitionUser = transitionUser;
     }
     
-    // populate user list and leave out test account
+    // populate user list while leaving out test account
     private void populateTempUserList() throws SQLException {
         PreparedStatement statement = null;
         String query = "SELECT user.userId, user.userName, user.active, user.password FROM user "
@@ -104,9 +139,9 @@ public class UserListController implements Initializable {
 
             if (results.next() == false) {
                 System.out.println("No users found.");
-                Alert alert = new Alert(AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setContentText("No users were found. Please create a user.");
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("User List Empty");
+                alert.setContentText("No users were found. Please create a new user.");
                 alert.showAndWait();
             } else {
                 while (results.next()) {
@@ -146,6 +181,16 @@ public class UserListController implements Initializable {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+    
+    // error control alert for user selection
+    private void selectError() {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText("User Selection Error");
+        alert.setContentText("You have not selected a user to modify.");
+        alert.initModality(Modality.APPLICATION_MODAL);
+        alert.showAndWait();
     }
     
     @Override
